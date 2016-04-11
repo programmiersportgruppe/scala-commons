41dc2c069021c615ce6967d9abf0a6891eec7c12
Ben Barnard <barnardb@gmail.com>
2016-04-11 23:35:46 +0200

    Documentation massage
    With trivial type change.

--- basics/src/main/scala/org/programmiersportgruppe/scala/commons/basics/Futures.scala
+++ basics/src/main/scala/org/programmiersportgruppe/scala/commons/basics/Futures.scala
@@ -7,32 +7,27 @@ object Futures {
 
   implicit class AugmentedFuture[T](val self: Future[T]) extends AnyVal {
 
-    /** Converts the "inner Try" of the Future to an option, discarding the
-      * Throw.
+    /** Converts the "inner Try" of the Future to an option,
+      * wrapping successful values in Some and replacing failures with None.
       *
       * This is useful if the result will just not be used in the case of an
       * error.
       */
     def toOption(implicit executor: ExecutionContext): Future[Option[T]] = {
-      //            self
-      //                .map(Some(_))
-      //                .recover { case t: Throwable => None }
-      // Scala 2.11 is happy, 2.10 is not; I think it's a bug in 2.10 that we would have to work around
-
-      // Here's the more efficient Promise implementation
       val p = Promise[Option[T]]()
       self.onComplete(result => p.success(result.toOption))
       p.future
     }
 
 
-    /** Causes a side effect in case the future fails. */
+    /** Causes an asynchronous side effect in case the future fails.
+      *
+      * Note that the original future is returned,
+      * meaning that it will be completed before the action has completed.
+      */
     def handleFailure(action: Throwable => Unit)(implicit executor: ExecutionContext): Future[T] = {
       self.onFailure { case t => action(t) }
       self
-
-      // Is this method useful? it's equivalent to
-      // self.andThen { case Failure(t) => action(t) }
     }
 
   }
--- basics/src/main/scala/org/programmiersportgruppe/scala/commons/basics/Tries.scala
+++ basics/src/main/scala/org/programmiersportgruppe/scala/commons/basics/Tries.scala
@@ -7,12 +7,12 @@ object Tries {
   /** Extension methods for Try */
   implicit class AugmentedTry[T](val self: Try[T]) extends AnyVal {
 
-    /** Execute action if the Try is a Failure.
+    /** Execute an action if the Try is a Failure.
       *
       * Can be used to cause a side effect in case something went wrong,
       * e.g. for logging before doing a Try.toOption.
       */
-    def onFailure(action: Throwable => Unit): Try[T] = {
+    def onFailure(action: Throwable => Unit): self.type = {
       self match {
         case Failure(t) => action(t)
         case _ =>
